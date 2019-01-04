4962f85f536c6c89ed7dbafff0f457cf9c997996
zch345 <1344972653@qq.com>
2019-01-04 16:46:04 +0800

    idea+spring测试

---
+++ src/main/java/com/zch/test/testspringboot/TestspringbootApplication.java
@@ -0,0 +1,14 @@
+package com.zch.test.testspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestspringbootApplication.class, args);
+    }
+
+}
+
